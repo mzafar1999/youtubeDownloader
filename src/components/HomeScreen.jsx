15790822270c3635fbfa1c24359cf22e67332325
mzafar <m.zafarhayatzada@gmail.com>
2022-01-11 07:35:10 +0500

    updated

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,37 +1,67 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import "./homescreen.css"
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import {FcInfo} from 'react-icons/fc'
+import "./homescreen.css";
+
 const HomeScreen = () => {
-    const [url, setUrl] = useState("");
-    const handleSubmit = (e)=>{
-        e.preventDefault()
-    }
-    
+  const [url, setUrl] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="container parent-all-content">
       <form onSubmit={handleSubmit}>
-       <div className="d-flex main-logo">
-       <img className="youtube-logo" src="./images/logo.png" alt="logo" />
-        <div className="mr-2 title">
-        <p className="h4 text-white title-youtube">Youtube Downloader</p>
-        <p className="text-white" >
-          Download Audio e.g. mp3 and Video e.g. mp4 formats
-        </p>
+        <div className="d-flex main-logo">
+          <img className="youtube-logo" src="./images/logo.png" alt="logo" />
+          <div className="mr-2 title">
+            <p className="h4 text-white title-youtube">Youtube Downloader</p>
+            <h5 className="text-white">
+              Download Audio e.g. mp3 and Video e.g. mp4 formats
+            </h5>
+            <p className="text-white">
+              <b><em>Creaded by M.Zafar</em></b>
+            </p>
+            
+        
+          <div >
+            <p  style={{
+                color: "rgb(161, 216, 197)",
+                marginLeft: "5px",
+                display: 'inline'
+              }}>Reach me out {'=>'} </p>
+          <a
+              style={{
+                fontSize: "30px",
+                color: "rgb(161, 216, 197)",
+                marginLeft: "5px",
+              }}
+              target="_blank"
+              href="https://zafar-portfolio.herokuapp.com"
+            >
+              <FcInfo />
+            </a>
+          </div>
+          </div>
         </div>
-       </div>
-     
+
         <input
           type="text"
           id="defaultFormLoginEmailEx"
           className="form-control mb-5 "
           value={url}
-          onChange={(e)=>setUrl(e.target.value)}
+          onChange={(e) => setUrl(e.target.value)}
           placeholder="Paste youtube link here to download"
         />
       </form>
-      {url?
-        <iframe className="iframe" scrolling="no" src={`https://loader.to/api/card/?url=${url}`}></iframe>
-      :null}
+      {url ? (
+        <iframe
+          className="iframe"
+          scrolling="no"
+          src={`https://loader.to/api/card/?url=${url}`}
+        ></iframe>
+      ) : null}
     </div>
   );
 };
